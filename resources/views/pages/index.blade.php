892bbdcc1ad19e43ff863a158fc5e20292c25e0b
viraj lakshitha <virajlakshitha39@gmail.com>
2019-05-01 13:22:23 +0530

    a

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta https-equiv="X-UA-Compatible" content="ie=edge">
     <title>Viraj Gunathilaka</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
