f75f74fdecc7d47c445bf8379dd3cce1ef1e365b
viraj lakshitha <virajlakshitha39@gmail.com>
2019-05-09 20:05:03 +0530

    Modified .env file to support https

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -6,16 +6,16 @@
     <meta https-equiv="X-UA-Compatible" content="ie=edge">
     <title>Viraj Gunathilaka</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="{{ secure_asset('css/app.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/css.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('css/bootstrap.min.css') }}" rel="stylesheet">
-    <link href="{{ secure_asset('img/11.jpg') }}" rel="icon">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/css.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+    <link href="{{ asset('img/11.jpg') }}" rel="icon">
 </head>
 <body>
     <div class="container">
         <div class="row">
             <div class="col-6">
-                <img src="/img/11.jpg" height="65%" width="55%">
+                <img src="/img/11.jpg" height="75%" width="55%">
             </div>
             <div class="col-6">
                     I'm Viraj Lakshitha Gunathilaka
