88f614bdd117e3aebff3c5ada205d98b211a154a
viraj lakshitha <virajlakshitha39@gmail.com>
2019-05-09 22:36:55 +0530

    Modified Home Page

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,19 +12,68 @@
     <link href="/css/bootstrap.min.css" rel="stylesheet">
     <link href="/img/11.jpg" rel="icon">
 </head>
+
 <body>
-    <div class="container">
-        <div class="row">
-            <div class="col-6">
-                <img src="/img/11.jpg" height="75%" width="55%">
+    <div class="container-fluid">
+        <div class="row align-middle text-white fixed-top" id="navigation">
+            <div class="col-1">
+                <img src="/img/11.jpg" height="70%" width="90%">
             </div>
-            <div class="col-6">
-                    I'm Viraj Lakshitha Gunathilaka
+            <div class="col-3 navs">I'm Viraj Lakshitha Gunathilaka</div>
+            <div class="col-4 text-right navs">Contact Me</div>
+            <div class="col-1 text-right navs">About Me</div>
+            <div class="col-1 text-right navs">My Projects</div>
+            <div class="col-1 text-right navs">
+                <span><a href="">Login/Register</a></span>
+            </div>
+        </div>
+        <div class="row" id="content2">
+            <div class="col-12">
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img class="d-block" src="/img/slider1.jpg" alt="First slide" width="1366px" height="600px">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/slider2.jpg" alt="Second slide" width="1366px" height="600px">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/slider3.jpg" alt="Third slide" width="1366px" height="600px">
+                        </div>
+                        <div class="carousel-item">
+                            <img class="d-block w-100" src="/img/slider4.gif" alt="Third slide" width="1366px" height="600px">
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
 </body>
+<footer class="container-fluid">
+    <div class="row">
+        <div class="col-1"></div>
+        <div class="col-5">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-8" id="footer_list">
+                        <ul>
+                            <li><a href="">My Projects</a></li>
+                            <li><a href="">About Me</a></li>
+                            <li><a href="">Contact Me</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row text-white text-center">
+        <div class="col-12">
+            2019 All Rights Reserved
+        </div>
+    </div>
+</footer>
 </html>
+
 <!-- Scripts -->
 <script src="/js/app.js"></script>
 <script src="/js/bootstrap.min.js"></script>
