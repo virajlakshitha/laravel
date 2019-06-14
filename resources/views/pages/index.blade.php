1141d0cd6d5cc8c8768fd92acae47675776e4743
viraj lakshitha <virajlakshitha39@gmail.com>
2019-06-14 12:39:13 +0530

    Modified intrface

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -15,8 +15,8 @@
 </head>
 
 <body>
-    <div class="container-fluid">
-        
+    <div class="container-fluid pb-4">
+
         @extends('pages.menu_bar')
 
         <div class="row" id="content2">
@@ -36,6 +36,60 @@
                             <img class="d-block w-100" src="/img/slider4.gif" alt="Third slide" width="1366px" height="600px">
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+        <div id="content">
+            <div class="container">
+                <div class="row mt-4 bg-primary">
+                    <div class="col-12 pt-3 pl-5 pb-2 text-white font-weight-bold">
+                        <h4>What do I provide ?</h4>
+                    </div>
+                </div>
+                <div class="card pl-5 shadow">
+                    <div class="card-body">
+                        <dl>
+                            <dt>Websites</dt>
+                            <dd>- PHP Laravel + CSS + HTML + Javascript + MySQL</dd>
+                            <dd>- PHP Laravel + React JS + MySQL</dd>
+                            <dd>- PHP CodeIgniter + CSS + HTML + Javascript + MySQL</dd>
+                            <dd>- Angular JS + NodeJS + MongoDB</dd>
+                            <dd>- Angular JS + Spring Boot + MongoDB</dd>
+                            <dd>- React JS + NodeJS + MongoDB</dd>
+                            <dd>- React JS + Spring Boot + MongoDB</dd>
+
+                            <dt>Mobile Apps</dt>
+                            <dd>- Android</dd>
+                            <dd>- Ionic</dd>
+                            <dd>- React Native</dd>
+
+                            <dt>Logo Design</dt>
+                        </dl>
+                    </div>
+                </div>
+                <div class="row mt-4 shadow bg-primary">
+                    <div class="col-12 pt-3 pl-5 pb-2 text-white font-weight-bold">
+                        <h4>My Curriculum Vitae</h4>
+                    </div>
+                </div>
+                <div class="card pl-5 pr-5 shadow">
+                    <div class="card-body">
+                        <span>Viraj Lakshitha Gunathilaka</span>
+                        <div class="float-right">11</div>
+                    </div>
+                </div>
+                <div class="row mt-4 shadow bg-primary">
+                    <div class="col-12 pt-3 pl-5 pb-2 text-white font-weight-bold">
+                        <h4>Portfolio</h4>
+                    </div>
+                </div>
+                <div class="card pl-5 pr-5 shadow">
+                    <div class="card-body">
+                        <span>Hospital Management System</span>
+                    </div>
+                    <div class="card-body">
+                            <span>Hospital Management System</span>
+                        </div>
                 </div>
             </div>
         </div>
